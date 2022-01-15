e36cc991119971755cef87bcdfa4440f2db48443
tipsman360 <97181692+tipsman360@users.noreply.github.com>
2022-01-15 23:08:01 +0600

    Done

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Electrical Engineer", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["Electrical Engineer", "Web-Developer", "App-Developer", "Designer", "IT Expert"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Electrical Engineer", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["Electrical Engineer", "Web-Developer", "App-Developer", "Designer", "IT Expert"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
